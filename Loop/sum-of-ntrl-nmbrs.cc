66daf06c854f1b2309fbd7aa842ba60bc82c8e2f
kmochy013 <kmochy013@gmail.com>
2024-10-12 11:22:05 +0800

    saved

--- Loop/sum-of-ntrl-nmbrs.cc
+++ Loop/sum-of-ntrl-nmbrs.cc
@@ -1,2 +1,18 @@
 #include<iostream>
 using namespace std;
+
+int main(){
+
+    int n, sum = 0;
+
+    cout << "Here are the first 10 natural numbers:\n";
+    
+    for( n = 1; n <= 10; n++){
+        cout << n << " ";
+
+        sum = sum + n;
+
+    }
+    cout << "\nHere is the sum of first 10 natural numbers: " << sum << "";
+
+}
